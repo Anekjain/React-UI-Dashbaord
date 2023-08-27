51b79b8b04a3569a1ce38531b2c276f41674d05b
Anek Porwal <anekporwal@Aneks-MacBook-Air.local>
2023-08-27 14:34:19 +0530

    Menu Changes-Made

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,40 @@
-import './menu.scss';
+import { Link } from "react-router-dom";
+import "./menu.scss";
 
 function Menu() {
   return (
-    <div className="menu">Menu</div>
-  )
+    <div>
+      <div className="menu">
+        <div className="item">
+          <span className="title">Main</span>
+
+          <Link to="/" className="listItem">
+            <img src="/home.svg" alt=""></img>
+            <span className="listItemTitle">Home</span>
+          </Link>
+
+          <Link to="/" className="listItem">
+            <img src="/profile.svg" alt=""></img>
+            <span className="listItemTitle">Profile</span>
+          </Link>
+        </div>
+
+        <div className="item">
+          <span className="title">Main2</span>
+
+          <Link to="/" className="listItem">
+            <img src="/home.svg" alt=""></img>
+            <span className="listItemTitle">Home2</span>
+          </Link>
+
+          <Link to="/" className="listItem">
+            <img src="/profile.svg" alt=""></img>
+            <span className="listItemTitle">Profile2</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      Home - NEED TO CREATE MENU ITEM LIST DATA AND MAKE IT DYNAMIC
+    </div>
+  );
 };
 
 export default Home;
